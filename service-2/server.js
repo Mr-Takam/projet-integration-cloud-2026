5c0024e03c28765323c69aebb47e9c4fe7b15de5
Cyril Takam <127041301+Mr-Takam@users.noreply.github.com>
2026-02-20 22:22:04 +0000

    feat: integration of PostgreSQL database and Service 2 connection (Target 16/20)

--- service-2/server.js
+++ service-2/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({
-    service: "Service 2",
-    message: "Réponse envoyée avec succès au Service 1 !",
-    timestamp: new Date()
-  });
+// Configuration de la connexion via le DNS interne de Kubernetes
+const pool = new Pool({
+  user: 'admin',
+  host: 'postgres-service', // Le nom du service K8s créé juste au-dessus !
+  database: 'projetdb',
+  password: 'admin123',
+  port: 5432,
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const dbRes = await pool.query('SELECT NOW() as db_time');
+    
+    res.json({
+      service: "Service 2",
+      message: "Connexion à PostgreSQL RÉUSSIE !",
+      database_time: dbRes.rows[0].db_time,
+      timestamp: new Date()
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Erreur de connexion BDD", details: error.message });
+  }
 });
 
 app.listen(port, () => {
