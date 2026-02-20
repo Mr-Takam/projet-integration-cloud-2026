bfd49ee451867607ef53b0d03e48349a5755f4b2
Cyril Takam <127041301+Mr-Takam@users.noreply.github.com>
2026-02-20 21:16:21 +0000

    feat: service-1 deployed to kubernetes with docker hub image (Target 10/20)

---
+++ service-1/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Hello Cyril et Abdou ! Service 1 opérationnel.",
+    timestamp: new Date()
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Service 1 en écoute sur le port ${port}`);
+});
